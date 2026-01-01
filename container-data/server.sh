f7a46de0d2896a54a6c01ba0279b1b1499936f95
ALTERRQ <alterrq@gmail.com>
2026-01-01 20:32:37 +0100

    Introducing RedLoader support in branch: redloader

--- container-data/server.sh
+++ container-data/server.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+# Install RedLoader
+if [ -f "/data/SonsOfTheForestDS.exe" ]; then
+    if [ ! -d "_Redloader" ]; then
+        echo ">>> Downloading and unpacking RedLoader.zip"
+        wget -qO /data/RedLoader.zip "https://github.com/ToniMacaroni/RedLoader/releases/latest/download/RedLoader.zip"
+        unzip -qo /data/RedLoader.zip
+        rm /data/RedLoader.zip
+        echo ">>> Redloader installed"
+    else
+        echo ">>> Redloader already installed"
+    fi
+else
+    echo ">>> Skipping: The game is not yet installed"
+fi
+
+# Launch and check if log filtering is enabled
 if [ "$FILTER_SHADER_AND_MESH_AND_WINE_DEBUG" = "true" ]; then
     # Start the server without output buffering and pipe through grep to filter out shader warnings
     WINEDEBUG=-all stdbuf -oL -eL wine SonsOfTheForestDS.exe -userdatapath $SERVER_CONFIG_DIR 2>&1 | \
