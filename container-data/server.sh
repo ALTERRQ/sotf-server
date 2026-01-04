04115400584b4b179e4d1ed980402e548fb64e39
ALTERRQ <alterrq@gmail.com>
2026-01-04 13:39:46 +0100

    Added logging without any filtering to game server

--- container-data/server.sh
+++ container-data/server.sh
@@ -3,9 +3,10 @@
 set -e
 
 if [ "$FILTER_SHADER_AND_MESH_AND_WINE_DEBUG" = "true" ]; then
-    # Start the server without output buffering and pipe through grep to filter out shader warnings
+    # Start the server without output buffering and pipe through grep to filter out shader warnings + log without filtering
     WINEDEBUG=-all stdbuf -oL -eL wine SonsOfTheForestDS.exe -userdatapath $SERVER_CONFIG_DIR 2>&1 | \
+    tee "$SERVER_CONFIG_DIR/game_server.log" | \
     stdbuf -oL grep -v -E ".*WARNING: Shader.*|.*ERROR: Shader.*|.*No mesh data available for mesh.*|.*Couldn't create a Convex Mesh from source.*|.*The referenced script.*|.*Could not find video decode shader pass.*"
 else
-    wine SonsOfTheForestDS.exe -userdatapath $SERVER_CONFIG_DIR
+    wine SonsOfTheForestDS.exe -userdatapath $SERVER_CONFIG_DIR 2>&1 | tee "$SERVER_CONFIG_DIR/game_server.log"
 fi
