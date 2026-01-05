fa78e165e4a5ec13b2d56993e367037d60e7c3ba
ALTERRQ <alterrq@gmail.com>
2026-01-05 23:16:53 +0100

    Quick and dirty fix for the RedLoader issue with linux headless servers. RedLoader version bumped up to V0.8.6 and working. Added raw and filtered logs. Little improvement in RedLoader install detection. The logs have limited width now, due to the dirty fix. I will investigate this issue further later.

--- container-data/server.sh
+++ container-data/server.sh
@@ -4,25 +4,29 @@ set -e
 
 # Install RedLoader
 if [ -f "/data/SonsOfTheForestDS.exe" ]; then
-    if [ ! -d "/data/_RedLoader" ]; then
+    if [ -d "/data/_RedLoader" ] || [ -d "/data/_Redloader" ]; then
+        echo ">>> A Redloader version is already installed"
+    else
         echo ">>> Downloading and unpacking RedLoader.zip (version: $REDLOADER_VERSION)"
         wget -qO /data/RedLoader.zip "https://github.com/ToniMacaroni/RedLoader/releases/download/$REDLOADER_VERSION/RedLoader.zip"
         unzip -qo /data/RedLoader.zip
         rm /data/RedLoader.zip
         echo ">>> Redloader $REDLOADER_VERSION installed"
-    else
-        echo ">>> Redloader $REDLOADER_VERSION is already installed"
     fi
 else
     echo ">>> Skipping: The game is not yet installed"
 fi
 
-# Launch and check if log filtering is enabled
+LOG_FILE="$SERVER_CONFIG_DIR/game_server.log"
+RAW_LOG_FILE="$SERVER_CONFIG_DIR/game_server_raw.log"
+
 if [ "$FILTER_SHADER_AND_MESH_AND_WINE_DEBUG" = "true" ]; then
-    # Start the server without output buffering and pipe through grep to filter out shader warnings + log without filtering
-    WINEDEBUG=-all stdbuf -oL -eL wine SonsOfTheForestDS.exe -userdatapath $SERVER_CONFIG_DIR 2>&1 | \
-    tee "$SERVER_CONFIG_DIR/game_server.log" | \
-    stdbuf -oL grep -v -E ".*WARNING: Shader.*|.*ERROR: Shader.*|.*No mesh data available for mesh.*|.*Couldn't create a Convex Mesh from source.*|.*The referenced script.*|.*Could not find video decode shader pass.*"
+    # Save raw output, save and show filtered output + quick fix for redloader issue from V0.8.0 and above
+    script -q -c "WINEDEBUG=-all stdbuf -oL -eL wine SonsOfTheForestDS.exe -userdatapath \"$SERVER_CONFIG_DIR\"" /dev/null 2>&1 | \
+    tee "$RAW_LOG_FILE" | \
+    stdbuf -oL grep -v -E ".*WARNING: Shader.*|.*ERROR: Shader.*|.*No mesh data available for mesh.*|.*Couldn't create a Convex Mesh from source.*|.*The referenced script.*|.*Could not find video decode shader pass.*" | \
+    tee "$LOG_FILE"
 else
-    wine SonsOfTheForestDS.exe -userdatapath $SERVER_CONFIG_DIR 2>&1 | tee "$SERVER_CONFIG_DIR/game_server.log"
+    # Save and show all output + quick fix for redloader issue from V0.8.0 and above
+    script -q -c "wine SonsOfTheForestDS.exe -userdatapath \"$SERVER_CONFIG_DIR\"" /dev/null 2>&1 | tee "$LOG_FILE"
 fi
